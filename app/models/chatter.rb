f9ce86f98b6a194b87bac525c4a06552af282371
TammyLLC <me@TammyLLC.com>
2014-03-04 23:14:01 -0500

    trying to see why these commits weren't pushed up before

--- app/models/chatter.rb
+++ app/models/chatter.rb
@@ -1,7 +1,7 @@
 class Chatter < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :chittle
-	has_many :chit_response
+	has_many :chit_responses
 
 	validates_length_of :chit_response, :minimum => 1
 	validates_length_of :chit_response, :maximum => 350 
